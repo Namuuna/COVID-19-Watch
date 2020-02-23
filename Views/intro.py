7f835a22f78463e69b7dbfb9c7539147bb0eed07
Saurav <sauravmsingh@gmail.com>
2020-02-23 01:11:47 -0500

    updates

---
+++ Views/intro.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def view():
+    st.write("### Background Introduction")
+    st.image("https://www.sciencenews.org/wp-content/uploads/2020/02/020720_ac-jl-ts_coronavirus_feat-1028x579.jpg",use_column_width=True)
+    st.write('''
+    The novel (new) coronavirus that was first detected in Wuhan City, Hubei Province, China. On February 11, 2020, the World Health Organization named the disease coronavirus disease 2019 (COVID-19).
+    There are tens of thousands of cases of coronavirus disease in China with the virus reportedly spreading from the close contact between person-to-person, in particular through respiratory droplets from coughs and sneezes within a range of around 6 feet.
+    ''')
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import streamlit as st
-from viz import view
+
+from Views.intro import view as intro
+from Views.viz import view as viz
 
 
-def viewIntroduction():
-    st.write("### Background Introduction")
-    st.image("https://www.sciencenews.org/wp-content/uploads/2020/02/020720_ac-jl-ts_coronavirus_feat-1028x579.jpg",use_column_width=True)
-    st.write('''
-    The novel (new) coronavirus that was first detected in Wuhan City, Hubei Province, China. On February 11, 2020, the World Health Organization named the disease coronavirus disease 2019 (COVID-19).
-    There are tens of thousands of cases of coronavirus disease in China with the virus reportedly spreading from the close contact between person-to-person, in particular through respiratory droplets from coughs and sneezes within a range of around 6 feet.
-    ''')
-
-def viewGraph():
-    view()
 
 
 if __name__ == '__main__':
@@ -19,11 +11,13 @@ if __name__ == '__main__':
 
     st.sidebar.title("Menu")
 
-    menu = st.sidebar.selectbox("Features",("Introduction", "Graphs", "Risk Rate"))
+    menuOptions = ["Introduction","Graphs","Risk Rate"]
+
+    menu = st.sidebar.selectbox("Features",menuOptions)
 
     views = {
-    "Introduction"  : viewIntroduction,
-    "Graphs"        : view
+    "Introduction"  : intro,
+    "Graphs"        : viz
     }
 
     views[menu]()
